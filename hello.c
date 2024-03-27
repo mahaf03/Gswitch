97fd4941a3c112a00e80dfd36b0dfed5f2822dad
mahaf03 <mahsih@kth.se>
2024-03-27 17:30:16 +0100

    Update helloworld

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main(){
-    printf("Hello World");
 
+    printf("Hello World");
+    printf("Hello World");
     printf("Git test\n ");
     return 0;
 
