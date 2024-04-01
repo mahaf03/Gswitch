28aedc70d287e5b0b677e380877f825c6d708014
Hemming Sundelin <hemming@kth.se>
2024-04-01 18:03:59 +0200

    test new makefile

--- view/view.c
+++
@@ -1,6 +0,0 @@
-#include "view.h"
-#include "../source/defines.h"
-
-void render(SDL_Renderer *renderer, SDL_Texture *texture, Model *model) {
-    // Rendering logic here, as shown in previous code
-}
