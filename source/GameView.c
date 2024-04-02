0af819531e91665509841a361fe2543a9566bc99
miladkth <116656210+miladkth@users.noreply.github.com>
2024-04-02 17:46:45 +0200

    Ändrade skärmstorlek till 1000, 800

--- source/GameView.c
+++ source/GameView.c
@@ -3,7 +3,7 @@
 #include <SDL2/SDL_image.h>
 
 void initView(SDL_Renderer** renderer, SDL_Window** window, SDL_Texture** texture) {
-    *window = SDL_CreateWindow("GSwitch", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, 0);
+    *window = SDL_CreateWindow("GSwitch", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, 0);
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Surface* surface = IMG_Load("resources/ship.png");
     *texture = SDL_CreateTextureFromSurface(*renderer, surface);
