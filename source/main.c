3d1c147ebc86570fcbcc8f5788faba8d3588dcf2
jaksve <dr.svedling@gmail.com>
2024-04-15 23:15:50 +0200

    Revert "server and client"
    This reverts commit 88672329e349e29cc23ae9ffbe97aca322a66aba.
    
    revert

--- source/main.c
+++ source/main.c
@@ -4,35 +4,9 @@
 #include "GameView.h"
 #include "GameController.h"
 #include "Network.h"
-typedef struct receiver {
-    UDPsocket* sd;
-    int* playercount;
-    GameModel* models;
-}receiver;
-Uint32 callback ( Uint32 interval, void* name)
-    {
-        receiver rec = *(receiver*)name;
-        udpDataToClient message;
-        IPaddress addrr = clientReceivePacket(&message,rec.sd);
-        //printf("done!\n");
-        if(addrr.host !=0 && addrr.port !=0  )
-            {
-                printf("got package\n");
-                for(int i = 0; i< message.playercount; i++)
-                    {
-                        printf("\tplayer %d at %f %f\n",i,message.playerPositions[i].x,message.playerPositions[i].y);
-                        *rec.playercount= message.playercount;
-                        rec.models[i].x = message.playerPositions[i].x;
-                        rec.models[i].y = message.playerPositions[i].y;
-                    }
-            }
 
-
-        return interval;
-
-    }
 int main(int argv, char** args) {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
@@ -54,15 +28,9 @@ int main(int argv, char** args) {
     IPaddress srvadd;
     UDPpacket* pReceive;
     UDPpacket* p;
-    initNetwork_Client(&sd,&srvadd);
+    initNetwork_Client(&sd,&srvadd,pReceive);
     printf("Network initialized!\n");
-    receiver reccy;
-    reccy.models = models;
-    reccy.playercount = &playercount;
-    reccy.sd = &sd;
-    SDL_TimerID  timerID = SDL_AddTimer(1,callback,&reccy);
     //Allocate memory for UDP packets
-/*
     if (!(p = SDLNet_AllocPacket(512)))
         {
             fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
@@ -73,66 +41,52 @@ int main(int argv, char** args) {
             fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
             exit(EXIT_FAILURE);
         }
-        */
 
     bool closeWindow = false;
     while (!closeWindow) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             handleEvent(&event, &model, &closeWindow);
-        }
-        udpDataToServer testdata ={model.x,model.y,0};
-        if (model.x >= 400.f)
-            { testdata.status =3;}
-        clientSendPacket(testdata,&srvadd,&sd);
+            udpDataToServer testdata ={model.x,model.y,0};
+            //printf("sending data \n\t %f %f %d\n",testdata.xPos,testdata.yPos,testdata.status);
+            //send a message to server, if testdata.status == 3 then the server shuts down.
+            if (model.x >= 400.f)
+                { testdata.status =3;}
+            clientSendPacket(testdata,&srvadd,&sd);
 
-        /*
+            //printf("data sent!\n");
+        }
         udpDataToClient message;
         IPaddress addrr = clientReceivePacket(&message,&sd);
         if(addrr.host !=0 && addrr.port !=0  )
             {
-                printf("got package\n");
-                for(int i = 0; i< message.playercount; i++)
-                    {
-                        printf("\tplayer %d at %f %f\n",i,message.playerPositions[i].x,message.playerPositions[i].y);
-                        playercount = message.playercount;
-                        models[i].x = message.playerPositions[i].x;
-                        models[i].y = message.playerPositions[i].y;
-                    }
-            }
-*/
-        /*udpDataToClient message;
-        //IPaddress addrr = clientReceivePacket(&message,&sd);
-        IPaddress addrr = clientReceivePacket(&message,&sd);
-        if(addrr.host !=0 && addrr.port !=0  )
-            {
                 printf( "new message from %x:\n",addrr.host);
-                playercount = message.playercount;
-                for(int i = 0; i< message.playercount; i++)
-                    {
-                        printf("\tplayer %d at %f %f\n",i,message.playerPositions[i].x,message.playerPositions[i].y);
-                        models[i].x = message.playerPositions[i].x;
-                        models[i].y = message.playerPositions[i].y;
-                    }
+                    playercount = message.playercount;
+                    for(int i = 0; i< message.playercount; i++)
+                        {
+                            printf("\tplayer %d at %f %f\n",i,message.playerPositions[i].x,message.playerPositions[i].y);
+                            models[i].x = message.playerPositions[i].x;
+                            models[i].y = message.playerPositions[i].y;
+                        }
             }
-*/
         for (int i = 0; i<playercount;i++)
             {
+                models[i].x=message.playerPositions[i].x;
+                models[i].y=message.playerPositions[i].y;
                 updateModel(&models[i]);
             }
+
         updateModel(&model);
         
         // Uppdatera renderView-anropet för att inkludera bakgrundstexturen
-        //printf("playercount: %d \n",playercount);
         renderView(renderer, texture, bgTexture, &models[0],playercount);
-        if(playercount == 0)
-            renderView(renderer, texture, bgTexture, &model,1);
+        //renderView(renderer, texture, bgTexture, &model,1);
+
         SDL_Delay(1000 / 60); // Approximate 60 FPS
     }
 
     // Glöm inte att skicka bgTexture till closeView så att det också kan rensas upp
     closeView(renderer, window, texture, bgTexture);
-    closeNetwork_Client(&sd);
     SDL_Quit();
     return 0;
 }
--- source/server.c
+++ source/server.c
@@ -2,30 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include "SDL2/SDL_net.h"
-#include "SDL2/SDL_timer.h"
 #include "Network.h"
-typedef struct  sender{
-  udpDataToClient* data;
-  IPaddress* player;
-  UDPsocket * sd;
-} sender;
-Uint32 callback ( Uint32 interval, void* name)
-{
-  sender Data =  *(sender* )name;
-  serverSendPacket (*Data.data,Data.player,Data.sd);
-  return interval;
-}
-
 
 int main(int argc,char** argv)
   {
-    SDL_Init(SDL_INIT_TIMER);
     UDPsocket sd;
     UDPpacket* pSent;
     printf("opening server..\n");
     udpDataToClient dataSend= {};
     dataSend.status = 0;
-    dataSend.playercount = 0;
     IPaddress players[4];
             for (int i = 0;i<4;i++)
               {
@@ -38,18 +23,14 @@ int main(int argc,char** argv)
         fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
         exit(EXIT_FAILURE);
       }
-    sender sendy;
-    //sendy.data = &dataSend;
-    //sendy.players = &players[0];
-    //sendy.sd = &sd;
-    //SDL_TimerID  timerID = SDL_AddTimer(10,callback,&sendy);
+
     udpDataToServer message;
     int quit = 0;
     while (quit==0)
       {
-       IPaddress host;
-       host = serverReceivePacket(&message,&sd);
-        if( host.host != 0 && host.port !=0)
+        IPaddress host;
+         host = serverReceivePacket(&message,&sd);
+         if( host.host != 0 && host.port !=0)
           {
             int playerNo = 0;
             for (int i = 0;i<4;i++)
@@ -60,11 +41,6 @@ int main(int argc,char** argv)
                       {
                         dataSend.playercount++;
                         printf("Player %d connected! \n \t %x \n \t %d\n", i+1, host.host,host.port);
-                        sender sendy;
-                        sendy.data = &dataSend;
-                        sendy.player = &players[i];
-                        sendy.sd = &sd;
-                        SDL_TimerID  timerID = SDL_AddTimer(100/60,callback,&sendy);
                       }
                     players[i] = host;
                     playerNo = i+1;
@@ -76,16 +52,13 @@ int main(int argc,char** argv)
             dataSend.playerPositions[playerNo-1] = position;
             if (message.status == 3)
               {quit = 1;}
-            //printf ("playercount: %d \n", dataSend.playercount);
-            for (int i = 0; i < dataSend.playercount; i++)
+            for (int i = 0; i< dataSend.playercount; i++)
               {
-                //printf ("Sending!\n\t player %d\t %f, %f\n",i+1,dataSend.playerPositions[i].x,dataSend.playerPositions[i].y);
-                //serverSendPacket (dataSend,&players[i],&sd);
-                //printf("sent\n");
+                serverSendPacket (dataSend,&players[i],&sd);
               }
 
           }
-                 SDL_Delay(10); // Approximate 60 FPS
+                 SDL_Delay(50); // Approximate 60 FPS
       }
     closeNetwork_Server(&sd,pSent);
     printf("server closed..\n");
