9cea614fb46b185b226c8862c35c0860351ca6c9
Hemming Sundelin <hemming@kth.se>
2024-04-16 12:05:21 +0200

    fungerade collection detection, test åt alla håll --> ok

--- source/GameModel.c
+++ source/GameModel.c
@@ -1,10 +1,9 @@
-// GameModel.c
 #include "GameModel.h"
 #include "GameView.h"
 
 void initializeModel(GameModel* model) {
-    model->x = 200; // Half of WINDOW_WIDTH
-    model->y = 200; // Half of WINDOW_HEIGHT
+    model->x = 200;
+    model->y = 200;
     model->velocityX = model->velocityY = 0;
     model->up = model->down = model->left = model->right = false;
     model->collisionUp = model->collisionDown = model->collisionLeft = model->collisionRight = false;
@@ -15,34 +14,31 @@ int min(int a, int b) {
 }
 
 int checkCollision(SDL_Rect shipRect, SDL_Rect blockRect) {
-    // Check for no collision first
-    if (shipRect.x + shipRect.w <= blockRect.x ||  // Ship is to the left of block
-        shipRect.x >= blockRect.x + blockRect.w || // Ship is to the right of block
-        shipRect.y + shipRect.h <= blockRect.y ||  // Ship is above block
-        shipRect.y >= blockRect.y + blockRect.h) { // Ship is below block
-        return 0; // No collision
+    if (shipRect.x + shipRect.w <= blockRect.x ||
+        shipRect.x >= blockRect.x + blockRect.w ||
+        shipRect.y + shipRect.h <= blockRect.y ||
+        shipRect.y >= blockRect.y + blockRect.h) {
+        return 0;
     }
 
-    // Calculate overlaps on each side
     int overlapLeft = shipRect.x + shipRect.w - blockRect.x;
     int overlapRight = blockRect.x + blockRect.w - shipRect.x;
     int overlapTop = shipRect.y + shipRect.h - blockRect.y;
     int overlapBottom = blockRect.y + blockRect.h - shipRect.y;
 
-    // Find the least overlap to determine the collision side
     int minOverlap = min(min(overlapLeft, overlapRight), min(overlapTop, overlapBottom));
 
     if (minOverlap == overlapLeft) {
-        return 1; // Collision on the right side of the ship
+        return 1;
     } else if (minOverlap == overlapRight) {
-        return 2; // Collision on the left side of the ship
+        return 2;
     } else if (minOverlap == overlapTop) {
-        return 3; // Collision on the top of the ship (ship is below)
+        return 3;
     } else if (minOverlap == overlapBottom) {
-        return 4; // Collision on the bottom of the ship (ship is above)
+        return 4;
     }
 
-    return 0; // Fallback, should not be reached
+    return 0;
 }
 
 void handleCollision(GameModel* model, SDL_Rect shipRect, SDL_Rect* blockPositions, int numBlocks, SDL_Renderer* renderer, SDL_Texture* blockTexture) {
@@ -50,9 +46,26 @@ void handleCollision(GameModel* model, SDL_Rect shipRect, SDL_Rect* blockPositio
     
     for (int i = 0; i < numBlocks; i++) {
         int collisionResult = checkCollision(shipRect, blockPositions[i]);
-        // printf("Collision result: %d\n", collisionResult);
-        stopModel(model, collisionResult);
         
+        switch(collisionResult) {
+            case 1: // Collision on the right side of the ship
+                model->collisionLeft = true;
+                model->x -= shipRect.x + shipRect.w - blockPositions[i].x;
+                break;
+            case 2: // Collision on the left side of the ship
+                model->collisionRight = true;
+                model->x += blockPositions[i].x + blockPositions[i].w - shipRect.x;
+                break;
+            case 3: // Collision on the top of the ship (ship is below)
+                model->collisionDown = true;
+                model->y -= shipRect.y + shipRect.h - blockPositions[i].y;
+                break;
+            case 4: // Collision on the bottom of the ship (ship is above)
+                model->collisionUp = true;
+                model->y += blockPositions[i].y + blockPositions[i].h - shipRect.y;
+                break;
+        }
+
         if(collisionResult != 0){
             anyCollision = true;
         }
@@ -60,7 +73,7 @@ void handleCollision(GameModel* model, SDL_Rect shipRect, SDL_Rect* blockPositio
     }
 
     if (!anyCollision) {
-        model->collisionRight = false; // Reset collision state if no collisions detected
+        model->collisionRight = false;
         model->collisionLeft = false;
         model->collisionUp = false;
         model->collisionDown = false;
