c3f485bfb67d81e803b60646d2ff3a30d9c7d08d
mahaf03 <mahsih@kth.se>
2024-04-25 18:48:26 +0200

    server update till main

--- source/main.c
+++ source/main.c
@@ -145,10 +145,6 @@ int main(int argv, char **args)
             {
                 handleEvent(&event, &model, &closeWindow);
                 udpDataToServer testdata = {model.x, model.y, 0};
-                if (model.x >= 400.f)
-                {
-                    testdata.status = 3;
-                }
                 clientSendPacket(testdata, &srvadd, &sd);
             }
         }
